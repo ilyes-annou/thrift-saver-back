b5d44becd73aa2ccd5b5e84449d9b23b3f78a195
annoui <ilyesannou77@gmail.com>
2024-01-04 22:56:44 +0300

    anti XSS + request limit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express= require("express");
 const cors= require("cors");
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const app= express();
 const userRoutes= require("./services/user/controller");
 const spendingRoutes= require("./services/spending/controller");
@@ -8,6 +10,22 @@ const spendingRoutes= require("./services/spending/controller");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'trusted-scripts.com'],
+        styleSrc: ["style.com"],
+      },
+    })
+);
+// 100 requests per 15 minutes
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+});
+app.use(limiter);
 
 const mongoose= require("mongoose");
 const port= 3080;
